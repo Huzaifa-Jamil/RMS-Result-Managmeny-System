5353af8d38eafe8d46a136a94cc96bd1e7b4c2b9
rising-phenix <m30422328@gmail.com>
2026-05-26 02:44:03 +0500

    Add ScienceStudent subclass extending Student with science-specific grade calculation

---
+++ Student/ScienceStudent.java
@@ -0,0 +1,87 @@
+package Student;
+import Transcript.Transcript;
+
+public class ScienceStudent extends Student {
+
+    // Data Members Encapsulates
+    // No Data Members
+
+    // Static Data Member
+    static int totalScienceStudents = 0;
+
+    // Setter Method
+    public static void setTotalScienceStudents(int a) {
+        totalScienceStudents = a;
+    }
+
+    // Getter Method
+    public static int getTotalScienceStudents() {
+        return totalScienceStudents;
+    }
+
+    // Argument constructor method With Authentaction
+    public ScienceStudent(String a, String b, String c, Transcript d) {
+        super(a, b, c, d);
+        totalScienceStudents++;
+    }
+
+    // Zero Argument constructor method
+    public ScienceStudent() {
+        super();
+        totalScienceStudents++;
+    }
+
+    // Method to Get Student Type
+    public String getStudentType() {
+        return "Science Student";
+    }
+
+    // Method to Calculate Total Marks
+    public double calculateTotal() {
+        return getTranscript().getTotalMarks();
+    }
+
+    // Abstract Method Implementations
+    public double calculatePercentage() {
+        int totalCourses = getTranscript().getResultEntry().size();
+        if (totalCourses == 0) {
+            return 0.0;
+        }
+
+        double totalPossibleMarks = totalCourses * 100.0;
+        double obtainedMarks = getTranscript().getTotalMarks();
+
+        return ((obtainedMarks / totalPossibleMarks) * 100.0);
+    }
+
+    // Method to Calculate Grade
+    public String calculateGrade() {
+        if (getTranscript().getResultEntry().isEmpty()) {
+            return "N/A";
+        }
+        double GPA = getTranscript().getGPA();
+        if (GPA >= 4.00) {
+            return "A";
+        } else if (GPA >= 3.66) {
+            return "A-";
+        } else if (GPA >= 3.33) {
+            return "B+";
+        } else if (GPA >= 3.00) {
+            return "B";
+        } else if (GPA >= 2.66) {
+            return "B-";
+        } else if (GPA >= 2.33) {
+            return "C+";
+        } else if (GPA >= 2.00) {
+            return "C";
+        } else if (GPA >= 1.66) {
+            return "C-";
+        } else if (GPA >= 1.30) {
+            return "D+";
+        } else if (GPA >= 1.00) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+}
