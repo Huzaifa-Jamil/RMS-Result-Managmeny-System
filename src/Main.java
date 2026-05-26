3e47e202d6e5b269b48cf0b03953bd6e6c1a78e2
Huzaifa-Jamil <huzaifajamil.ke24@gmail.com>
2026-05-26 07:57:11 +0500

    Add new Main.java MainClass

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import ui.*;
+
+import javax.swing.Timer;
+import javax.swing.JProgressBar;
+import javax.swing.SwingUtilities;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            Splash_Screen_UI splashScreen = new Splash_Screen_UI();
+            JProgressBar progressBar = splashScreen.getProgressBar();
+
+            int duration = 3000;
+            int delay = 30;
+            int steps = duration / delay;
+
+            Timer timer = new Timer(delay, null);
+            timer.addActionListener(e -> {
+                int value = progressBar.getValue();
+                if (value < 100) {
+                    progressBar.setValue(value + 100 / steps);
+                } else {
+                    ((Timer)e.getSource()).stop();
+                    RMS_Result_Management_System_UI gui = new RMS_Result_Management_System_UI();
+                    gui.setVisible(true);
+                    splashScreen.dispose();
+                }
+            });
+            timer.start();
+        });
+    }
+}
