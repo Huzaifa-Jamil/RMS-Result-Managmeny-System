635cadf7f0dde552f16a4b3be1ac4473d0a88994
Huzaifa-Jamil <huzaifajamil.ke24@gmail.com>
2026-05-27 02:17:03 +0500

    Add ai folder to accomodate the executable of echo_AI

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
+import ui.*;
+
+import javax.swing.Timer;
 import javax.swing.JProgressBar;
 import javax.swing.SwingUtilities;
-import javax.swing.Timer;
-import ui.*;
 
 public class Main {
     public static void main(String[] args) {
