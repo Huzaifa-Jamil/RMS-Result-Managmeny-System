58f0e40a68e1864cf97cae708ee5615d9ab9f4d3
Huzaifa-Jamil <huzaifajamil.ke24@gmail.com>
2026-05-26 07:36:24 +0500

    Relocate Student Model along with childs from Student/ to src/models/

---
+++ src/models/Engineering_Student.java
@@ -0,0 +1,75 @@
+package models;
+
+public class Engineering_Student extends Student {
+
+    // Data Members Encapsulates
+    // No Data Members
+
+    // Setter Method
+
+    // Getter Method
+
+    // Argument constructor method With Authentaction
+    public Engineering_Student(String a, String b, String c, Transcript d) {
+        super(a, b, c, d);
+    }
+
+    // Zero Argument constructor method
+    public Engineering_Student() {
+        super();
+    }
+
+    // Method to Get Student Type
+    public String getStudentType() {
+        return "Engineering Student";
+    }
+
+    // Method to Calculate Total Marks
+    public double calculateTotal() {
+        return getTranscript().getTotalMarks();
+    }
+
+    // Abstract Method Implementations
+    public double calculatePercentage() {
+        int totalCourses = getTranscript().getResultEntry().size();
+        if (totalCourses == 0) {
+            return 0.0;
+        }
+
+        double totalPossibleMarks = totalCourses * 100.0;
+        double obtainedMarks = getTranscript().getTotalMarks();
+
+        return ((obtainedMarks / totalPossibleMarks) * 100.0);
+    }
+
+    // Method to Calculate Grade
+    public String calculateGrade() {
+        if (getTranscript().getResultEntry().isEmpty()) {
+            return "N/A";
+        }
+        double GPA = getTranscript().getGPA();
+        if (GPA >= 4.00) {
+            return "A";
+        } else if (GPA >= 3.66) {
+            return "A-";
+        } else if (GPA >= 3.33) {
+            return "B+";
+        } else if (GPA >= 3.00) {
+            return "B";
+        } else if (GPA >= 2.66) {
+            return "B-";
+        } else if (GPA >= 2.33) {
+            return "C+";
+        } else if (GPA >= 2.00) {
+            return "C";
+        } else if (GPA >= 1.66) {
+            return "C-";
+        } else if (GPA >= 1.30) {
+            return "D+";
+        } else if (GPA >= 1.00) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+}
