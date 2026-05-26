d54ebb3b14767293d596ae52bb9c4d1753458266
Huzaifa-Jamil <huzaifajamil.ke24@gmail.com>
2026-05-26 08:10:50 +0500

    Add Top Panel

---
+++ src/ui/Top_Panel_UI.java
@@ -0,0 +1,23 @@
+package ui;
+
+import java.awt.*;
+import javax.swing.BorderFactory;
+
+import javax.swing.JPanel;
+
+import utils.UI_Styles;
+
+public class Top_Panel_UI extends JPanel {
+
+    public Top_Panel_UI() {
+        createTopTitlePanel();
+    }
+
+    private void createTopTitlePanel() {
+        this.setLayout(new BorderLayout());
+        this.setBackground(Color.WHITE);
+        this.setPreferredSize(new Dimension(0, 10));
+        this.setBorder(BorderFactory.createEmptyBorder());
+        UI_Styles.makeFocusStealerRecursive(this);
+    }
+}
